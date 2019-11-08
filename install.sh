946cf768c53ec4a0d28c7715afc7a4679e43120f
Diëggho <diegghooliver@gmail.com>
2019-11-08 12:16:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/bash
 
-sudo pacman -S awesome-terminal-fonts gsimplecaln rofi xorg-xprop xorg-xset xorg-xsetroot
+echo "Installing fonts from Arch Linux repo"
+
+sudo pacman -S awesome-terminal-fonts ttf-dejavu --noconfirm --needed
+
+echo "################################################################"
+echo "####        Fonts from Arch Linux repo have been installed        ####"
+echo "################################################################"
+
+echo "Installing dependencies from Arch Linux repo"
+sudo pacman -S gsimplecaln rofi xorg-xprop xorg-xset xorg-xsetroot --noconfirm --needed
+
+echo "################################################################"
+echo "####        Dependencies from Arch Linux repo have been installed        ####"
+echo "################################################################"
+
+echo "Installing dwm"
 make -C dwm-src/
 sudo make install -C dwm-src/
 sudo mv dwm.desktop /usr/share/xsessions/
 cp -r .dwm/ ~/
 chmod +x ~/.dwm/*
+
+echo "################################################################"
+echo "####        Dwm has been installed        ####"
+echo "################################################################"
+
+echo echo "Installing st and slstatus"
+make -C st-src/
+make -C slstatus/
+sudo make install -C st-src/
+sudo make install -C slstatus/
+cp st-src/st.desktop ~/.local/share/applications/
+
+echo "################################################################"
+echo "####        Arc-Dwm has been successfully installed        ####"
+echo "################################################################"
