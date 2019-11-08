14977385bf7f762ea0c3b42c17a792c1036a022f
Diëggho <diegghooliver@gmail.com>
2019-11-08 15:16:42 -0300

    Update uninstall-arc-dwm.sh

--- uninstall-arc-dwm.sh
+++ uninstall-arc-dwm.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/bash
 
-echo "Uninstalling Arc-Dwm"
+echo "Uninstalling Dwm"
 sudo rm /usr/share/xsessions/dwm.desktop
 sudo make uninstall -C dwm-src/
 sudo make uninstall -C slstatus/
 sudo make uninstall -C st-src/
 
 echo "################################################################"
-echo "####        Arc-Dwm has been completely removed       ####"
-echo "################################################################"
-
-echo "Uninstalling Fonts from Arch Linux Repo"
-sudo pacman -R awesome-terminal-fonts ttf-dejavu --noconfirm --needed
-
-echo "################################################################"
-echo "####        Fonts from Arch Linux repo have been uninstalled        ####"
-echo "################################################################"
-
-echo "Uninstalling Dependencies from Arch Linux repo"
-sudo pacman -R dmenu gsimplecaln rofi xorg-xprop xorg-xset xorg-xsetroot --noconfirm --needed
-
-echo "################################################################"
-echo "####        Dependencies from Arch Linux repo have been uinstalled        ####"
+echo "####       Dwm has been completely removed       ####"
 echo "################################################################"
