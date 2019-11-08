5049d05839faededed83e003558d6a25a43f9d90
Diëggho <diegghooliver@gmail.com>
2019-11-08 10:35:13 -0300

    Update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
-###########################################################
-# This Dwm was patched with "autostart" and "autorestart" to be able to have an                     #
-# /user/share/xsession/dwm.desktop archive and be able to be started from a display manager  #
-# for this reason it's necessary to kill each process before actually start it                                   #
-# if you don't, the "autorestart" feature will run the autostart.sh script and                                #
-# will eventually start the following processes all over again.                                                       #
-###########################################################
+##########################################################
+# This Dwm was patched with "autostart" and "autorestart" to be able to have an                   #
+# /user/share/xsession/dwm.desktop archive and thus be able to be started                             #
+# from any display manager, for this reason it's necessary to kill each process                          #
+# before actually starting it. If you don't do that, then the "autorestart" feature will                 #
+# run the autostart.sh script and will eventually start the following processes all over again      #
+##########################################################
 
 pkill xset
 pkill udiskie
