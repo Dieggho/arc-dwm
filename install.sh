abd201087299bacaeb74a6dbc17bc672087c7198
Diëggho <diegghooliver@gmail.com>
2019-11-08 11:41:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+
+sudo pacman -S awesome-terminal-fonts gsimplecaln rofi xorg-xprop xorg-xset xorg-xsetroot
 make -C dwm-src/
 sudo make install -C dwm-src/
 sudo mv dwm.desktop /usr/share/xsessions/
