74b76540c6e0268e059646f0ba4fdbd9ed607fda
Diëggho <diegghooliver@gmail.com>
2019-11-08 12:38:47 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,7 @@ sudo rm /usr/share/xsessions/dwm.desktop
 sudo make uninstall -C dwm-src/
 sudo make uninstall -C slstatus/
 sudo make uninstall -C st-src/
+
 echo "################################################################"
 echo "####        Arc-Dwm has been completely removed       ####"
 echo "################################################################"
