5eae9160b2fd4aba69d3998a1202e9a1783d8b27
Diëggho <diegghooliver@gmail.com>
2019-11-08 10:37:05 -0300

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
- 
-##########################################################
-# This Dwm was patched with "autostart" and "autorestart" to be able to have an                   #
-# /user/share/xsession/dwm.desktop archive and thus be able to be started                             #
-# from any display manager, for this reason it's necessary to kill each process                          #
-# before actually starting it. If you don't do that, then the "autorestart" feature will                 #
-# run the autostart.sh script and will eventually start the following processes all over again      #
-##########################################################
 
+pkill feh
 pkill xset
+pkill /usr/bin/python
 pkill udiskie
 pkill slstatus
+
 
 /home/$USER/.fehbg &
 xset -dpms s off &
